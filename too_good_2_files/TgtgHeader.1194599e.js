e2c4b45c07495ae681c407a544621605686785cf
Esther Vendrell <esther.vendrell>
2023-04-16 14:08:21 +0200

    inicio

---
+++ too_good_2_files/TgtgHeader.1194599e.js
@@ -0,0 +1,55 @@
+import te from"./nuxt-link.268b8628.js";import oe from"./TgtgToggle.5347cac6.js";import"./nuxt.72dd13a5.js";import{T as $}from"./runtime-dom.esm-bundler.89c5e5b8.js";import{u as ae,a as ne}from"./router.2f85908e.js";import"./app.config.1f6fe35d.js";import{u as re,E as se}from"./Analytics.73c2e451.js";import{u as ie}from"./useDownloadApp.1a3b05d2.js";import{u as le}from"./useChangeRouteLocale.567985a6.js";import ce from"./tgtg_logo.2dd333b7.js";import"./TgtgStatsPromo.vue.16590ac7.js";import{d as _e}from"./TgtgSelect.bac466fc.js";import{a as de,u as ge,b as D,e as R,N as L}from"./baseConfig.6d96edc8.js";import{D as me,G as ue}from"./constants.7c0d7f52.js";import{u as pe}from"./faqs.3a2e3829.js";import{u as he}from"./Testimonial.4173d93a.js";import{u as ve}from"./countries.358ba54b.js";import{_ as O}from"./TgtgIcon.vue.cad7dd0b.js";import{_ as fe}from"./TgtgCountryLocaleSelector.vue.918c36dd.js";import{k as ke,r as N,f as _,b as Ce,l as be,c as Se,m as a,p as i,q as f,x as p,u as e,z as C,ad as c,y as b,F as S,am as T,ae as H,an as we,al as w,ak as h,ao as ye}from"./runtime-core.esm-bundler.c03fb5c6.js";import{_ as Le}from"./_plugin-vue_export-helper.c27b6911.js";import"./index.e5521a99.js";/* empty css                       */import"./defu.5a7c1433.js";import"./index.742a418e.js";import"./index.6f428c29.js";import"./paths.47577c01.js";import"./preload-helper.87151e19.js";/* empty css                                   *//* empty css                                    *//* empty css                               */import"./TgtgScrollDownArrow.c6f744c6.js";import"./TgtgHeroHeading.b7fb2fe5.js";import"./Components.07ddcaad.js";/* empty css                               */import"./TgtgSlider.da4a11b5.js";import"./TgtgPagination.129a2fa6.js";/* empty css                          *//* empty css                           *//* empty css                     */import"./index.d859d9d0.js";import"./_commonjsHelpers.edff4021.js";/* empty css                            *//* empty css                       *//* empty css                    */import"./TgtgImagePromo.c7b53096.js";/* empty css                                */import"./TgtgLeadRequestForm.vue.375ccc46.js";import"./cookie.9cc3efdd.js";import"./index.36486552.js";/* empty css                       *//* empty css                      *//* empty css                                */import"./TgtgPlainText.vue.92d4799c.js";/* empty css                      */import"./TgtgSliderBasic.b3cb380b.js";/* empty css                            *//* empty css                                        */import"./TgtgTabCards.vue.2f88024d.js";import"./TgtgTabs.3674a577.js";import"./TgtgTestimonialsSlider.vue.467699f0.js";import"./asyncData.b28a962e.js";import"./error.b2ec325e.js";/* empty css                      */import"./TgtgWysiwyg.vue.8c3ac793.js";/* empty css                    */import"./layout.5e1ed215.js";import"./utils.a8a1bba5.js";import"./nuxt-error-boundary.f4990a13.js";import"./client-only.e3303045.js";import"./dev-only.c88d966b.js";import"./server-placeholder.2700d376.js";import"./nuxt-loading-indicator.14a945d5.js";import"./page.7aeb0f31.js";import"./components.3a891a7e.js";import"./composables.fff436f9.js";import"./dynamic-import-helper.be004503.js";import"./fetchAllPages.c1ecba74.js";import"./deepMerge.e8daf263.js";const Ne=`<?xml version="1.0" encoding="utf-8"?>
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="20" viewBox="0 0 25 20">
+	<path
+        id="hamburger-line-1"
+        fill="none"
+        stroke="#000000"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2,2 l10.542,0 L23,2"/>
+	<path
+        id="hamburger-line-2"
+        fill="none"
+        stroke="#000000"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2,10 l10.542,0 h10.542"/>
+	<path
+        id="hamburger-line-3"
+        fill="none"
+        stroke="#000000"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M2,18 l10.542-0 L23,18"/>
+</svg>
+`,Te=`<?xml version="1.0" encoding="utf-8"?>
+<svg xmlns="http://www.w3.org/2000/svg" width="25" height="20" viewBox="0 0 25 20">
+    <path
+        id="hamburger-line-1"
+        fill="none"
+        stroke="#00FF00"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.5,2 l9.1,8 L21.5,2"/>
+    <path
+        id="hamburger-line-2"
+        fill="none"
+        stroke="#0000FF"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M12.5,10 L12.5,10 h0.042"/>
+    <path
+        id="hamburger-line-3"
+        fill="none"
+        stroke="#FF0000"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.5,18 l9.1-7.667 L21.5,18"/>
+</svg>
+`;function He(){const x=pe(),k=he();x.clearStore(),k.clearStore()}const xe={key:0,class:"tgtg-header__overlay"},Ee={class:"tgtg-header__wrapper tgtg-wrapper"},Ae={class:"tgtg-header__nav tgtg-header__nav--links"},Be={class:"tgtg-header__nav__list"},Me=["onClick"],Fe={key:0,class:"tgtg-header__subnav"},$e={class:"tgtg-header__subnav__wrapper"},De={class:"tgtg-header__subnav__title"},Re={class:"tgtg-header__subnav__list"},Oe={class:"tgtg-header__nav tgtg-header__nav--ctas"},Ie={key:0,class:"tgtg-header__nav__list"},ze={class:"tgtg-header__nav__link"},Ge={key:2},je=["innerHTML"],Pe=ke({__name:"TgtgHeader",setup(x){const k=ae(),E=ne(),r=de(),n=ge(),I=ve(),A=N(null),d=N(!1),g=N(""),B=_(()=>{var t;return k.params.locale?{name:"locale",params:{locale:(t=n.localeConfig)==null?void 0:t.localeCode}}:{name:"index"}}),M=_(()=>n.activeNavigation[D.HEADER][R.CTA]),z=_(()=>n.activeNavigation[D.HEADER][R.MAIN]),G=_(()=>r.segment!==L.UNSET),j=_(()=>r.segment===L.HOMEPAGE),P=_(()=>!!k.params.locale||k.params.locale!==ue),U=_(()=>{var t,s;return n.localeConfig?{countryCode:(t=n.localeConfig)==null?void 0:t.countryCode,localeCode:(s=n.localeConfig)==null?void 0:s.localeCode}:void 0}),V=_(()=>{var s,l,m,o;const t=I.getCountryByCode(((s=n.localeConfig)==null?void 0:s.countryCode)||"");return(t==null?void 0:t.localizations.length)===1?(l=n.localeConfig)==null?void 0:l.countryLabel:`${(m=n.localeConfig)==null?void 0:m.countryLabel} (${(o=n.localeConfig)==null?void 0:o.localeCode.split("-")[0].toUpperCase()})`}),q=t=>{var l,m;r.setActiveSegment(t);let s;t===L.USER?s={name:"locale-user",params:{locale:(l=n.localeConfig)==null?void 0:l.localeCode}}:s={name:"locale-business",params:{locale:(m=n.localeConfig)==null?void 0:m.localeCode}},E.push(s),v()},W=()=>{re({event:se.GENERIC_DOWNLOAD_CLICK,action:"click",label:"Header download CTA clicked"}),ie()},K=_(()=>r.getShowLocaleSelectorInHeader&&P.value),J=_(()=>"Find my local website"),Q=()=>{d.value=!d.value,r.setScrollBlockedStatus(d.value)},v=()=>{d.value=!1,r.setScrollBlockedStatus(d.value)},X=t=>{He(),E.push(le(t.localeCode)),v()},y=()=>{var t;d.value||document.documentElement.style.setProperty("--header-height",(((t=A.value)==null?void 0:t.offsetHeight)||0)+"px")},Y=t=>{g.value=g.value===t?"":t,r.setScrollBlockedStatus(!!g.value)},Z=(t,s)=>{!s.target.classList.contains("tgtg-header__subnav__parent")&&!s.target.classList.contains("tgtg-header__nav__link__trigger")&&(g.value="",r.setScrollBlockedStatus(!1))};return Ce(()=>n.activeNavigation.header,()=>{setTimeout(y,500)},{immediate:!0}),be(()=>{window.addEventListener("resize",y)}),Se(()=>{window.removeEventListener("resize",y)}),(t,s)=>{const l=te,m=oe;return a(),i(S,null,[f($,{name:"fade-in-out"},{default:p(()=>[e(g)?(a(),i("div",xe)):C("",!0)]),_:1}),c("header",{ref_key:"tgtgHeader",ref:A,class:H(["tgtg-header",{"tgtg-header--is-homepage":e(j),"tgtg-header--open":e(d)}])},[c("div",Ee,[f(l,{to:e(B),class:"tgtg-header__logo"},{default:p(()=>[c("img",{src:ce,alt:"Too Good To Go logo",class:"tgtg-header__logo__image",onClick:v})]),_:1},8,["to"]),e(G)?(a(),b(m,{key:0,class:"tgtg-header__segment-toggle",options:e(r).availableSegments,value:e(r).segment,onChange:q},null,8,["options","value"])):C("",!0),c("nav",Ae,[c("ul",Be,[(a(!0),i(S,null,T(e(z),(o,F)=>(a(),i("li",{class:H(["tgtg-header__nav__link",{"tgtg-header__nav__link--active":!1}]),style:we({minWidth:o&&o.label.length>10?"125px":"unset"}),key:`header-nav-${F}-${e(n).activeLocale}-${e(r).segment}`},[o.children&&o.children.length?(a(),i(S,{key:0},[c("span",{class:"tgtg-header__nav__link__item tgtg-header__subnav__parent",onClick:u=>Y(o.identifier)},[w(h(o.label)+" ",1),f(e(O),{name:"chevron-down_icon",class:H(["tgtg-header__nav__link__trigger",{"tgtg-header__nav__link__trigger--active":o.identifier===e(g)}])},null,8,["class"])],8,Me),f($,{name:"slide-down"},{default:p(()=>[o.identifier===e(g)?ye((a(),i("div",Fe,[c("div",$e,[c("p",De,h(o.label),1),c("ul",Re,[(a(!0),i(S,null,T(o.children,(u,ee)=>(a(),i("li",{key:`header-subnav-${F}-${ee}-${e(n).activeLocale}-${e(r).segment}`,class:"tgtg-header__subnav__link"},[u.identifier?(a(),b(e(O),{key:0,name:u.identifier,class:"tgtg-header__subnav__link__icon"},null,8,["name"])):C("",!0),f(l,{to:u.url,class:"tgtg-header__subnav__link__item",onClick:v},{default:p(()=>[w(h(u.label),1)]),_:2},1032,["to"])]))),128))])])])),[[e(_e),u=>Z(o.identifier,u)]]):C("",!0)]),_:2},1024)],64)):(a(),b(l,{key:1,to:o&&o.url,class:"tgtg-header__nav__link__item",onClick:v},{default:p(()=>[w(h(o.label),1)]),_:2},1032,["to"]))],4))),128))])]),c("nav",Oe,[e(M).length?(a(),i("ul",Ie,[(a(!0),i(S,null,T(e(M),o=>(a(),i("li",ze,[o.identifier===e(me)?(a(),i("button",{key:0,class:"tgtg-header__cta tgtg-button",type:"button",onClick:W},h(o.label),1)):(a(),b(l,{key:1,to:o.url,class:"tgtg-header__cta tgtg-button",onClick:v},{default:p(()=>[w(h(o.label),1)]),_:2},1032,["to"]))]))),256))])):C("",!0)]),e(K)?(a(),b(e(fe),{key:1,class:"tgtg-header__lang-selector",placeholder:e(V),value:e(U),onChange:X},null,8,["placeholder","value"])):(a(),i("div",Ge,[f(l,{to:e(B),class:"tgtg-header__cta-back-to-locale tgtg-button"},{default:p(()=>[w(h(e(J)),1)]),_:1},8,["to"])]))]),c("div",{innerHTML:e(r).isSafari&&e(d)?e(Te):e(Ne),class:"tgtg-header__menu-trigger",onClick:Q},null,8,je)],2)],64)}}}),ho=Le(Pe,[["__scopeId","data-v-431727db"]]);export{ho as default};
---
+++ too_good_2_files/TgtgIcon.16662cfe.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./TgtgIcon.vue.cad7dd0b.js";import"./preload-helper.87151e19.js";import"./dynamic-import-helper.be004503.js";import"./runtime-core.esm-bundler.c03fb5c6.js";export{o as default};
